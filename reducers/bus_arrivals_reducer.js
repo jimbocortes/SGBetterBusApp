9c051c50546df022d78a9ecb818e4ecf5f051810
Jimbo Cortes <jimbo.cortes@gmail.com>
2017-10-05 01:54:26 +0800

    extract the bus results array

--- reducers/bus_arrivals_reducer.js
+++ reducers/bus_arrivals_reducer.js
@@ -1,9 +1,9 @@
 import { FETCH_BUS_ARRIVAL } from '../actions/types';
 
-export default function(state = null, action) {
+export default function(state = [], action) {
   switch (action.type) {
     case FETCH_BUS_ARRIVAL:
-      return action.payload;
+      return action.payload.Services;
     default:
       return state;
   }
